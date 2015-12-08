a5e579a91bf194bb926be06ba8ab657e8c0d3305
ErinUC <erinuc@gmail.com>
2015-12-08 16:37:38 -0500

    Finish user edit, update, destroy actions

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name:  "",
@@ -16,9 +17,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
-  test "successful edit" do
+  test "successful edit with friendly forwarding" do
   	get edit_user_path(@user)
-  	assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
   	name = "Foo Bar"
   	email = "foo@bar.com"
   	patch user_path(@user), user: { name: 	name,
@@ -28,8 +30,8 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   	assert_not flash.empty?
   	assert_redirected_to @user
   	@user.reload
-  	assert_equal name, @user.name
-  	assert_equal email, @user.email
+  	assert_equal name,   @user.name
+  	assert_equal email,  @user.email
 	end 
 
 end
